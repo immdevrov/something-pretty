bec858e4b444263e06c1347dffa499fc4ae09948
Роман Вайсер <immdevrov@gmail.com>
2020-04-21 22:10:40 +0300

    feat: add Toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './app.scss';
 import Button from './components/button.jsx';
+import Toggle from './components/toggle';
 
 function App () {
   return (
     <div className="app">
       <Button />
+      <Toggle />
     </div>
   );
 }
---
+++ src/components/toggle.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../styles/toggle.scss';
+
+export default class Toggle extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      toggled: false
+    };
+  }
+
+  handleClick (e) {
+    e.preventDefault();
+    this.setState(state => ({
+      toggled: !state.toggled
+    }));
+  }
+
+  render () {
+    return (
+      <div className="toggle">
+        <div
+          className="toggle__status-field"
+          style={ this.state.toggled ? {} : { opacity: .3 } }
+        >
+          { this.state.toggled ? 'ON' : 'OFF' }
+        </div>
+        <div className="toggle__switcher-field">
+          <div
+            className="toggle__switcher"
+            onClick={e => this.handleClick(e)}
+            style={ this.state.toggled ? { transform: 'scale(-1, 1)' } : {} }
+          >
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
