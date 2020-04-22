cd995bc948c5346ea1f7e3265bcb04cd2256f294
Роман Вайсер <immdevrov@gmail.com>
2020-04-22 23:14:46 +0300

    feat: remade button icon to backgrounds

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import '../styles/button.scss';
-import defaultButtonIcon from '../icons/default-button-icon.svg';
-import pressedButtonIcon from '../icons/pressed-button-icon.svg';
 
 export default function Button () {
   return (
     <button
       className="button"
     >
-      <img
-        className="button__icon--pressed"
-        alt="GO"
-        src={pressedButtonIcon}
-      />
-      <img
-        className="button__icon--nonpressed"
-        alt="GO"
-        src={defaultButtonIcon}
-      />
+      <div className="button__icon"></div>
     </button>
   );
 }
