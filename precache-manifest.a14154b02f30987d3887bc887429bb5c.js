05effb96192809606495cf8bf38aac41f08fe799
Роман Вайсер <immdevrov@gmail.com>
2020-05-13 21:18:34 +0300

    Updates

---
+++ precache-manifest.a14154b02f30987d3887bc887429bb5c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e78c2cb34f1e1edd0c84dade27e6bea0",
+    "url": "/something-pretty/index.html"
+  },
+  {
+    "revision": "3e67aef925b99b8467d8",
+    "url": "/something-pretty/static/css/main.999f36ea.chunk.css"
+  },
+  {
+    "revision": "baa4ea409e4416f237d5",
+    "url": "/something-pretty/static/js/2.b97a226e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/something-pretty/static/js/2.b97a226e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e67aef925b99b8467d8",
+    "url": "/something-pretty/static/js/main.bd7b9d3b.chunk.js"
+  },
+  {
+    "revision": "369cbda996528175b2cd",
+    "url": "/something-pretty/static/js/runtime-main.23c6bb52.js"
+  },
+  {
+    "revision": "d2491a55e1ddf6031551e94a74a17513",
+    "url": "/something-pretty/static/media/default-button-icon-dark.d2491a55.svg"
+  },
+  {
+    "revision": "dd1900d43361ec10e4c8aa68131a28ef",
+    "url": "/something-pretty/static/media/default-button-icon.dd1900d4.svg"
+  },
+  {
+    "revision": "937d09242f63f7727e1bfae19a34d102",
+    "url": "/something-pretty/static/media/logo.937d0924.svg"
+  },
+  {
+    "revision": "08771a8e31137435d22063757a6de6af",
+    "url": "/something-pretty/static/media/off-toggle-icon.08771a8e.svg"
+  },
+  {
+    "revision": "01c42167c5438c95a713f216b5dded5f",
+    "url": "/something-pretty/static/media/pressed-button-icon-dark.01c42167.svg"
+  },
+  {
+    "revision": "a27b94a327bcc1585dd54a19831ef714",
+    "url": "/something-pretty/static/media/pressed-button-icon.a27b94a3.svg"
+  }
+]);
